2aee31c875b19f3698fb3e944faa59a6e1c407c3
Andre Cruz <andremiguelcruz@msn.com>
2019-11-25 00:22:25 +0000

    chore: wip

--- index.js
+++ index.js
@@ -1,54 +1,43 @@
 'use strict';
 
-const buildConfig = (keys, initialConfig, prefix, { removePrefixes }) => {
-    const re = new RegExp(`^${prefix}`, 'i');
+const set = require('lodash/get');
 
-    const config = keys.reduce((acc, key) => {
-        if (re.test(key)) {
-            const name = removePrefixes ? key.replace(prefix, '') : key;
-
-            return {
-                ...acc,
-                [name]: process.env[key],
-            };
-        }
-
-        return acc;
-    }, initialConfig);
-
-    return config;
+const keyMappings = {
+    public: 'publicRuntimeConfig',
+    server: 'serverRuntimeConfig',
+    build: 'env',
 };
 
-/**
- * NextJS plugin to pass environment variables to NextJS's configuration (`publicRuntimeConfig`, `serverRuntimeConfig` and `env`).
- *
- * @param {Object} options - The plugin options.
- * @param {string} options.publicPrefix - Prefix of variables to lookup and then pass to publicRuntimeConfig.
- * @param {string} options.serverPrefix - Prefix of variables to lookup and then pass to serverRuntimeConfig.
- * @param {string} options.buildPrefix - Prefix of variables to lookup and then pass to buildRuntimeConfig.
- * @param {string} options.removePrefixes - Option to remove prefix when passing variables to config.
- */
-const withEnv = (options = {}) => (nextConfig = {}) => {
-    const {
-        publicPrefix = 'NEXT_PUBLIC_',
-        serverPrefix = 'NEXT_SERVER_',
-        buildPrefix = 'NEXT_BUILD_',
-        removePrefixes = false,
-    } = options;
+const withEnv = (vars) => (nextConfig = {}) => {
+    const envConfig = vars.reduce((envConfig, env) => {
+        const key = keyMappings[env.type];
 
-    let {
-        publicRuntimeConfig = {},
-        serverRuntimeConfig = {},
-        env = {},
-    } = nextConfig;
+        if (!key) {
+            throw new Error(`Unknown environment variable type '${env.type}'`);
+        }
 
-    const envKeys = Object.keys(process.env);
+        const value = process.env[env.name];
 
-    publicRuntimeConfig = buildConfig(envKeys, publicRuntimeConfig, publicPrefix, { removePrefixes });
-    serverRuntimeConfig = buildConfig(envKeys, serverRuntimeConfig, serverPrefix, { removePrefixes });
-    env = buildConfig(envKeys, env, buildPrefix, { removePrefixes });
+        set(envConfig, `${env.type}.${env.name}`, value != null ? value : env.default);
 
-    return { ...nextConfig, publicRuntimeConfig, serverRuntimeConfig, env };
+        return envConfig;
+    }, {
+        publicRuntimeConfig: { ...nextConfig.publicRuntimeConfig },
+        serverRuntimeConfig: { ...nextConfig.serverRuntimeConfig },
+        env: { ...nextConfig.env },
+    });
+
+    return {
+        ...nextConfig,
+        ...envConfig,
+        webpack: (config, options) => {
+            if (typeof nextConfig.webpack === 'function') {
+                return nextConfig.webpack(config, options);
+            }
+
+            return config;
+        },
+    };
 };
 
 module.exports = withEnv;
